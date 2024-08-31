a48ad6b65d978e84ed7cbce3ada14ead3a0145e8
hdquidano28 <hdquidano@hotmail.es>
2024-08-30 21:59:42 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let domain = " ";
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let x = 0; x < adj.length; x++) {
+      for (let y = 0; y < noun.length; y++) {
+        domain += "<li>" + pronoun[i] + adj[x] + noun[y] + ".com" + "</li>";
+      }
+    }
+  }
+  document.getElementById("excuse").innerHTML = domain;
 };
